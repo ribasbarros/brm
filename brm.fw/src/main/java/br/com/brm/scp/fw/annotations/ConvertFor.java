6360df35afdc9dad2e2f23e9b094972352236256
Ribas <ribas.barros@gmail.com>
2015-05-17 17:49:54 -0300

    Corrigido bug de buid

--- brm.fw/src/main/java/br/com/brm/scp/fw/annotations/ConvertFor.java
+++ brm.fw/src/main/java/br/com/brm/scp/fw/annotations/ConvertFor.java
@@ -3,11 +3,9 @@ package br.com.brm.scp.fw.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-import br.com.brm.scp.fw.test.dto.ClassTestD;
-
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ConvertFor {
 
-	Class<ClassTestD> value();
+	Class<?> value();
 
 }
